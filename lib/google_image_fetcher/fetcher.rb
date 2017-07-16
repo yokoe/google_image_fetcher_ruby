ceb66377f00665107f53089f025877fa7ad70e96
Sota Yokoe <sota@kreuz45.com>
2017-07-16 20:27:47 +0900

    Use URI.encode_www_form to generate search url.

--- lib/google_image_fetcher/fetcher.rb
+++ lib/google_image_fetcher/fetcher.rb
@@ -1,10 +1,17 @@
+require "uri"
 module GoogleImageFetcher
   class Fetcher
     class << self
       def search_url(query)
         api_key = ENV["GOOGLE_API_KEY"]
         engine_id = ENV["SEARCH_ENGINE_ID"]
-        url = "https://www.googleapis.com/customsearch/v1?key=#{api_key}&cx=#{engine_id}&q=#{query}&searchType=image"
+        params = {
+          "key" => api_key,
+          "cx" => engine_id,
+          "q" => query,
+          "searchType" => "image"
+        }
+        url = "https://www.googleapis.com/customsearch/v1?#{URI.encode_www_form(params)}"
       end
 
       def store(dir_path, image_url)
